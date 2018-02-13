23a3b4f3c536ca81aa579ee84a990441d3f9cc10
Rich Song <richasong@gmail.com>
2018-02-12 20:10:09 -0500

    Getting transaction data; Interpolating tx hashes to view; Checkpoint

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      balance: 0
+      balance: 0,
+      totalReceived: 0,
+      totalTransactions: 0,
+      txs: []
     }
   }  
 
@@ -15,25 +18,43 @@ class App extends Component {
     this.getBalance();
   }
 
+  convertToBtc = (balance) => {
+    return balance/100000000;
+  }
+
   getBalance = () => {
     var addr = '17CUX3NGq2EeLwjXHAU95y3TjoRRvBmrbR'
     var url = `https://blockchain.info/multiaddr?cors=true&active=${addr}`;
-    
+    // var url2 = `https://blockchain.info/rawaddr/${addr}?cors=true`
+
     fetch(url)
       .then(response => response.json())
       .then(data => {
-        const finalBalance = data.wallet.final_balance;
-        const convertedBitcoinValue = finalBalance/100000000;
-        const convertedDollarValue = convertedBitcoinValue * 8880;
+        const finalBalance = this.convertToBtc(data.wallet.final_balance);
+        const totalReceived = this.convertToBtc(data.wallet.total_received);
+        const totalTransactions = data.wallet.n_tx;
+        const arrayOfTxs = [];
+
+        for (let i = 0; i < data.txs.length; i++) {
+          arrayOfTxs.push(data.txs[i]);
+        };
+
         this.setState({ 
-          balance: convertedDollarValue.toFixed(2)
+          balance: finalBalance,
+          totalReceived: totalReceived,
+          totalTransactions: totalTransactions,
+          txs: arrayOfTxs
         })
+
+        console.log(data);
       })
   };
 
   render() {
-    // console.log('test', this.state.balance);
-    console.log('props', this.props);
+    // console.log('balance', this.state.balance);
+    // console.log('totalTrans', this.state.totalTransactions);
+    // console.log('received', this.state.totalReceived);
+    // console.log(this.state.txs.length);
 
     return (
       <div className="App">
@@ -41,9 +62,23 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          {this.state.balance}
-        </p>
+
+        <div className="App-intro">
+          Summary
+          <div>{this.state.totalTransactions}</div>
+          <div>{this.state.balance}</div>
+          <div>{this.state.totalReceived}</div>
+        </div>
+
+        <div className="App-intro">
+          Transactions
+          <div>
+            {this.state.txs.map((tx, idx) => {
+              return (<span key={idx}> {tx.hash} </span>)
+            })}
+          </div>
+        </div>
+
       </div>
     );
   }
